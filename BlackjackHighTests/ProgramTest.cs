92d898b0081b90a239a4b69341cce248116ce121
adriaannortje <adriaannortje@gmail.com>
2025-06-04 21:36:09 +0200

    tests using xUnit

---
+++ BlackjackHighTests/ProgramTest.cs
@@ -0,0 +1,52 @@
+using System.Reflection;
+
+namespace BlackjackHighTests
+{
+    public class ProgramTest
+    {
+        private static string InvokeBlackjackHighest(string[] hand)
+        {
+            var blackjackType = typeof(BlackjackHigh.Blackjack);
+            var method = blackjackType.GetMethod("BlackjackHighest", BindingFlags.NonPublic | BindingFlags.Static);
+            return (string)method.Invoke(null, new object[] { hand });
+        }
+
+        [Theory]
+        [InlineData(new string[] { "ace", "eight", "ace" }, "below ace")]
+        [InlineData(new string[] { "ace", "nine", "ace" }, "blackjack ace")]
+        [InlineData(new string[] { "two", "three", "ace", "king" }, "below king")]
+        [InlineData(new string[] { "king", "four", "ten" }, "above king")]
+        [InlineData(new string[] { "ten", "jack" }, "below jack")]
+        [InlineData(new string[] { "ace", "queen" }, "blackjack ace")]
+        [InlineData(new string[] { "five", "six" }, "below six")]
+        public void BlackjackHighest_ValidHands_ReturnsExpected(string[] hand, string expected)
+        {
+            var result = InvokeBlackjackHighest(hand);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void BlackjackHighest_InvalidCard_ThrowsArgumentException()
+        {
+            var ex = Assert.Throws<TargetInvocationException>(() => InvokeBlackjackHighest(new string[] { "ten", "invalidcard" }));
+            Assert.IsType<ArgumentException>(ex.InnerException);
+            Assert.Contains("invalid card in hand", ex.InnerException.Message);
+        }
+
+        [Fact]
+        public void BlackjackHighest_EmptyHand_ThrowsArgumentException()
+        {
+            var ex = Assert.Throws<TargetInvocationException>(() => InvokeBlackjackHighest(new string[] { }));
+            Assert.IsType<ArgumentException>(ex.InnerException);
+            Assert.Contains("hand cannot be null or empty", ex.InnerException.Message);
+        }
+
+        [Fact]
+        public void BlackjackHighest_NullHand_ThrowsArgumentException()
+        {
+            var ex = Assert.Throws<TargetInvocationException>(() => InvokeBlackjackHighest(null));
+            Assert.IsType<ArgumentException>(ex.InnerException);
+            Assert.Contains("hand cannot be null or empty", ex.InnerException.Message);
+        }
+    }
+}
---
+++ BlackjackHighTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BlackjackHighTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
